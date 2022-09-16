aee2d0b5fa1ce32ff19fa7fb08821ac87d527878
Zachery-Liu <61861732+Zachery-Liu@users.noreply.github.com>
2022-09-16 13:08:56 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,13 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/Lianmoe/Lianmoe-Tutorial",
+    "use_repository_button": True,
+}
+
+
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
