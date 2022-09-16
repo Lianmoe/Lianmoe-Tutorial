76602e0bcf47f9de32b87239860b7a67342cf9c9
Zachery-Liu <61861732+Zachery-Liu@users.noreply.github.com>
2022-09-16 14:46:17 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'myst_parser'
 ]
 
+myst_enable_extensions = ["deflist"]
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
