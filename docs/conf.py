287c24e6d4969a2dae0d7f02f6f27a958ddbbf30
Zachery-Liu <61861732+Zachery-Liu@users.noreply.github.com>
2022-09-17 18:22:53 +0800

    disable linkify

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ myst_enable_extensions = [
     "fieldlist",
     "html_admonition",
     "html_image",
-    "linkify",
+    #"linkify",
     "replacements",
     "smartquotes",
     "strikethrough",
